7601ce30027ec665876382d5f9dd4395aa2240df
shuntia <shunta@koga.us>
2025-01-20 01:19:31 -0800

    created int-returning compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let args: std::vec::Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("invalid arg count!")
+    }
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", args.get(1).unwrap());
+    println!("  ret");
+    return;
 }
