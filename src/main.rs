80f3cb06f55da1abafd8fe1075f6de3085869148
shuntia <shunta@koga.us>
2025-01-24 11:31:59 -0800

    sync with linux

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,39 @@ fn main() {
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", args.get(1).unwrap());
+    let target = args[1][..].to_owned();
+    let mut it = target.chars();
+    let mut buf = "".to_owned();
+    while let c = it.next() {
+        match c {
+            Some(ch) => match ch {
+                '+' => break,
+                '-' => break,
+                cha => buf.push(cha),
+            },
+            None => panic!("unexpected error while reading"),
+        }
+    }
+    println!("  mov rax, {}", buf);
+    while let Some(c) = it.next() {
+        match c {
+            '+' => println!("  add rax, {}", parseint(&mut it).unwrap()),
+            '-' => println!("  sub rax, {}", parseint(&mut it).unwrap()),
+            _ => panic!("unexpected char"),
+        }
+    }
     println!("  ret");
     return;
+}
+fn parseint(s: &mut std::str::Chars) -> Result<u64, std::num::ParseIntError> {
+    let mut ret: String = Default::default();
+    while let Some(c) = s.clone().next() {
+        if c.is_digit(10) {
+            ret.push(c);
+            s.next();
+        } else {
+            break;
+        }
+    }
+    ret.parse()
 }
